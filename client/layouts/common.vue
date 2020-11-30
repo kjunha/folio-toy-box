fea7f814921de5c35248bb26bfc759fc1e8704f8
kjunha <kjunha77@gmail.com>
2020-11-30 22:57:50 +0900

    add template

--- client/layouts/common.vue
+++ client/layouts/common.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <!-- Header -->
+  <div class="subpage">
     <header id="header">
       <div class="inner">
-        <a href="/" class="logo"><strong>Projection</strong> by TEMPLATED</a>
+        <a href="index.html" class="logo"
+          ><strong>Projection</strong> by TEMPLATED</a
+        >
         <nav id="nav">
-          <a href="/">Home</a>
-          <a href="#">Generic</a>
-          <a href="/elements">Elements</a>
+          <a href="index.html">Home</a>
+          <a href="generic.html">Generic</a>
+          <a href="elements.html">Elements</a>
         </nav>
         <a href="#navPanel" class="navPanelToggle"
           ><span class="fa fa-bars"></span
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,73 +1,82 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    	<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Welcome to Projection</h1>
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<span class="icon fa-car"></span>
+							<h3>Aliquam</h3>
+							<p>Suspendisse amet ullamco</p>
+						</div>
+
+						<div>
+							<span class="icon fa-camera"></span>
+							<h3>Elementum</h3>
+							<p>Class aptent taciti ad litora</p>
+						</div>
+
+						<div>
+							<span class="icon fa-bug"></span>
+							<h3>Ultrices</h3>
+							<p>Nulla vitae mauris non felis</p>
+						</div>
+
+					</div>
+
+					<footer>
+						<a href="#" class="button">Get Started</a>
+					</footer>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<div class="image round">
+								<img src="~/assets/images/pic01.jpg" alt="Pic 01" />
+							</div>
+							<header>
+								<h3>Lorem ipsum<br /> dolor amet nullam</h3>
+							</header>
+							<p>Morbi in sem quis dui placerat ornare. Pellentesquenisi<br />euismod in, pharetra a, ultricies in diam sed arcu. Cras<br />consequat  egestas augue vulputate.</p>
+							<footer>
+								<a href="#" class="button">Learn More</a>
+							</footer>
+						</article>
+						<article>
+							<div class="image round">
+								<img src="~/assets/images/pic02.jpg" alt="Pic 02" />
+							</div>
+							<header>
+								<h3>Sed feugiat<br /> tempus adipicsing</h3>
+							</header>
+							<p>Pellentesque fermentum dolor. Aliquam quam lectus<br />facilisis auctor, ultrices ut, elementum vulputate, nunc<br /> blandit ellenste egestagus commodo.</p>
+							<footer>
+								<a href="#" class="button">Learn More</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
